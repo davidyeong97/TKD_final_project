efcb12f17d793d0dec9f76c41e9f8f6c33ffaadf
laukinping <80878606+laukinping@users.noreply.github.com>
2021-05-14 14:44:03 +0800

    creatAccount

---
+++ public/creatAccount/creatAccount.js
@@ -0,0 +1,21 @@
+const fetchLoginForm = document.querySelector('#fetchLoginForm')
+fetchLoginForm.addEventListener('submit', async(event) => {
+    event.preventDefault();
+    const form = event.target;
+    const formObject = {};
+    formObject['username'] = form.fetchUsername.value;
+    formObject['password'] = form.fetchPassword.value;
+    const response = await fetch('http://localhost:8080/login', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formObject),
+    })
+    let jsonResponse = await response.json();
+    document.querySelector('#fetch-area').innerHTML =
+        `<div>username: ${jsonResponse.username}</div>
+    <div>password: ${jsonResponse.password}</div>`
+
+
+})
